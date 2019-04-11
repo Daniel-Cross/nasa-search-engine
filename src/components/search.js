ab4b106f62929f07518de3e709eabbf9bc527436
Daniel Cross <crossy1686@gmail.com>
2019-04-11 15:28:05 +0200

    input clean up

--- src/components/search.js
+++ src/components/search.js
@@ -5,30 +5,17 @@ import '../styles/search.css';
 const Search = (props) => {
 	return (
 		<div className="Search">
-			<h1 className="title">NASA Search</h1>
 			<form action="submit" onSubmit={props.handleSubmit}>
 				<input type="text" placeholder="The Moon..." onChange={props.handleChange} />
 				<button>Search</button>
 			</form>
-
-			<div className="checkboxes">
-				<label htmlFor="images">
-					<input type="checkbox" images={props.images} onChange={props.handleImages} />Images
-				</label>
-				<label htmlFor="audio">
-					<input type="checkbox" audio={props.audio} onChange={props.handleAudio} />Audio
-				</label>
-			</div>
 		</div>
 	);
 };
 
 Search.protoTypes = {
 	handleChange: PropTypes.func.isRequired,
-	handleImages: PropTypes.func.isRequired,
-	handleAudio: PropTypes.func.isRequired,
-	images: PropTypes.bool.isRequired,
-	audio: PropTypes.bool.isRequired,
+	handleSubmit: PropTypes.func.isRequired,
 };
 
 export default Search;
