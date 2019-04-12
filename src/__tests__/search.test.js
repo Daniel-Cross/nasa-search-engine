c6455e454f7331dda9b06f76b9f1202ff109da2a
Daniel Cross <crossy1686@gmail.com>
2019-04-12 13:51:59 +0200

    testing added for search component

--- src/__tests__/search.test.js
+++ src/__tests__/search.test.js
@@ -4,8 +4,20 @@ import Search from '../components/search';
 import { shallow } from 'enzyme';
 import '../../testSetup';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<Search search="mock search" handleChange={() => null} handleSubmit={() => null} />, div);
-	ReactDOM.unmountComponentAtNode(div);
+describe('Search', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Search handleChange={() => null} handleSubmit={() => null} />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it('renders the correct prop types', () => {
+		const mockHandler = jest.fn();
+		const testInput = shallow(<Search handleChange={mockHandler} handleSubmit={mockHandler} />);
+
+		expect(mockHandler).not.toHaveBeenCalled();
+		testInput.find('input').simulate('change');
+		testInput.find('input').simulate('submit');
+		expect(mockHandler).toHaveBeenCalled();
+	});
 });
